a4ddbea4777c87f93090ff9a4db3881055db9d67
Kingcedru <byishimocedrick@gmail.com>
2024-06-21 19:59:30 +0200

    added styles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  template: '<h1>Navbar component</h1>',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
 })
 export class AppNavbar {}
