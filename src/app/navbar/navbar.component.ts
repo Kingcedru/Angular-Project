52558675919fb80060086a5332d822be08274590
Kingcedru <byishimocedrick@gmail.com>
2024-06-21 17:33:04 +0200

    navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  template: '<h1>Navbar component</h1>',
+})
+export class AppNavbar {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AppNavbar } from './app/navbar/navbar.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppNavbar, appConfig).catch((err) => console.error(err));
