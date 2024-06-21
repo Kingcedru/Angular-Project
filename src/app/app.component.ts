92da49b3ea197198583e2aae920941705fbf0a0d
Kingcedru <byishimocedrick@gmail.com>
2024-06-21 20:30:25 +0200

    added header component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
